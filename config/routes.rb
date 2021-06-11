dcd594e2b71aa635135056ea5ddc67bf710eafdf
phamdunggit <pham.dung.wk@gmail.com>
2021-06-11 14:15:02 +0700

    chapter 7 sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
   get 'static_pages/home'
   get 'static_pages/help'
-  get 'static_pages/sign_up'
+  get 'signup', to: 'users#new'
+  post "/signup", to: "users#create"
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
